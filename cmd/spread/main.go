d545ad5f8f9b5e00a5c3d9e2a8c2a0b9bd93a027
Genelet <genelet@gmail.com>
2025-03-30 20:57:05 -0700

    spread is done

--- cmd/spread/main.go
+++ cmd/spread/main.go
@@ -7,10 +7,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/genelet/winter/acl"
 	"github.com/genelet/winter/dsp"
-
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/genelet/winter/match"
+	"github.com/nats-io/nats.go"
 )
 
 func usage() {
@@ -33,17 +36,61 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	nc := sc.Nc
 	defer sc.Close()
 
-	filewriters, err := NewFileWriters(sc.C.Spread)
-	if err != nil {
+	top := sc.C.Spread
+	if err := os.MkdirAll(top, os.ModePerm); err != nil {
 		log.Fatal(err)
 	}
 
 	log.Printf("Listening on [%s]", nc.ConnectedUrl())
-	err = filewriters.ReceiveLogs(nc)
+	successchan := make(chan bool)
+	errchan := make(chan error)
+
+	_, err = nc.Subscribe("*", func(m *nats.Msg) {
+		switch m.Subject {
+		case dsp.SUBJECTRequest:
+			return
+		case dsp.SUBJECTResponse:
+			return
+		case dsp.SUBJECTAttribute:
+			return
+		case dsp.SUBJECTWinLoss:
+			return
+		default:
+		}
+
+		filename := strings.ReplaceAll(m.Subject, ":", "/")
+		dir, base := filepath.Split(filename)
+		if strings.HasPrefix(dir, acl.HashNamePubmap) ||
+			strings.HasPrefix(dir, match.HashNameAudience) ||
+			strings.HasPrefix(dir, match.HashNameSlot) ||
+			strings.HasPrefix(dir, match.HashNameCreative) {
+			var err error
+			if err = os.MkdirAll(fmt.Sprintf("%s/%s", top, dir), os.ModePerm); err == nil {
+				var w *os.File
+				if w, err = os.OpenFile(fmt.Sprintf("%s/%s/%s", top, dir, base), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666); err == nil {
+					defer w.Close()
+					_, err = w.Write(m.Data)
+				}
+			}
+			if err != nil {
+				errchan <- err
+			}
+		}
+		successchan <- true
+	})
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	for {
+		select {
+		case <-successchan:
+		case errs := <-errchan:
+			log.Printf("error: %v", errs)
+		}
 	}
 }
